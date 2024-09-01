9ff848fb16fcb30b81fe19f9cafabdfb19436c89
Peter Feher <peter.feher@pftechs.com>
2024-09-01 01:38:05 +0100

    completed

--- AccountAntAPI/Migrations/20240901000330_collections_archive.Designer.cs
+++ AccountAntAPI/Migrations/20240901000330_collections_archive.Designer.cs
@@ -3,6 +3,7 @@ using AccountAntAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AccountAntAPI.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240901000330_collections_archive")]
+    partial class collections_archive
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AccountAntAPI/Models/Collection.cs
+++ AccountAntAPI/Models/Collection.cs
@@ -12,6 +12,7 @@ namespace AccountAntAPI.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool Archived { get; set; }
         public ICollection<Item> Items { get; set; }
     }
 
